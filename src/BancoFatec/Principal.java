3ce76204b5a6c87c92b0f35548ff968f91c41c6e
Debora-Carvalho <deboravolc2@gmail.com>
2024-09-03 10:36:55 -0300

    adicionar ArrayList e operações

--- src/BancoFatec/Principal.java
+++ src/BancoFatec/Principal.java
@@ -3,6 +3,7 @@
  */
 package BancoFatec;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -15,38 +16,48 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
+		Conta c1 = new Conta(new Cliente("Sirley", "1197854-6320"), 5000.00);
+		Conta c2 = new Conta(new Cliente("Anahi", "1195588-6320"), 1000.00);
+		ArrayList<Conta> lista = new ArrayList<Conta>();
+		lista.add(c1);
+		lista.add(c2);
 		String menu;
 		int op = 0;
 		double valor;
-		menu = "\n 1.Depositar. \n 2.Sacar. \n 3.Transferir. \n 4.Mostrar dados. \n 0.Sair \n Digite a opção desejada.";
-		
+		menu = "\n 1.Depositar. \n 2.Sacar. \n 3.Transferir. \n 4.Listar. \n 5.Criar nova conta. \n 0.Sair \n Digite a opção desejada.";
+
 		do {
 			System.out.println(menu);
 			op = entrada.nextInt();	
-			
+
 			switch (op) {
 			case 1: {
-				
+				break;
 			}
 			case 2: {
-				
+				break;
 			}
 			case 3: {
-				
+				break;
 			}
 			case 4: {
-				
+				break;
+			}
+			case 5: {
+				break;
 			}
 			case 0: {
-				
+				System.out.print("Saindo do sistema...");
+				break;
 			}
 			default: {
-				
+				System.out.print("Opção inválida!\n Retornando ao menu...");
+				break;
 			}
 			}
 		} while (op != 0);
-		
-		
+
+
 	}
 
 }
